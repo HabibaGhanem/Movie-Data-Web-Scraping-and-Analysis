7c630a2cce9d0719e26c347e4ca1b1e36a0f8951
Aliaa Mahmoud Shawky Tamer | Ejada Systems Ltd. <ashawqy@ejadacairo.local>
2023-09-27 17:41:31 +0300

    sorted data

--- sorted_dataset.py
+++ sorted_dataset.py
@@ -10,7 +10,7 @@ merged_dataset.head(20)
 #sorted_dataset
 
 merged_dataset = pd.read_csv('/content/merged_movie_data.csv')
-sorted_dataset = merged_dataset.sort_values(by='Title',ascending=True)
+sorted_dataset = merged_dataset.sort_values(by = ['Title', 'MoviemeterRating', 'IMDbRating'])
 sorted_dataset.reset_index(drop=True)
 sorted_dataset.to_csv('sorted_dataset.csv',index=False)
 
