2d904d156df839a0dc33f0f2d214934a2937ce23
Habiba Essam  Ghanem | Ejada Systems Ltd. <hganem@ejadacairo.local>
2023-09-27 20:31:42 +0300

    adding project files

--- IMDb_Scraping.py
+++ IMDb_Scraping.py
@@ -37,7 +37,7 @@ class IMDbSpider(scrapy.Spider):
         movie_rank_list = response.xpath('//tr[contains(@class,"row")]/td[1][@align="center"]/text()').extract()
         rating_list = response.xpath('//td[4]/text()').extract()
         
-        dict = {'Title':title_list,'ReleaseYear':release_year_list, 'MovieRank':movie_rank_list, 'IMDbRating':rating_list}
+        dict = {'Title':title_list,'ReleaseYear':release_year_list, 'IMDbRank':movie_rank_list, 'IMDbRating':rating_list}
         df = pd.DataFrame(dict)
         df.to_csv('IMDb_data.csv',index=False)
 
